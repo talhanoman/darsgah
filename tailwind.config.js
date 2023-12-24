9056f4aeb473882662af8abd9bd314c6a2afd14c
Talha Noman <talhanoman61@gmail.com>
2023-12-24 21:39:18 +0500

    Colors Design System Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
 
   content: ["./App.{js,jsx,ts,tsx}", "./<custom-folder>/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        "blue" : "#0071BC",
+        "yellow" : "#FFCB45",
+        "orange" : "#FF7644",
+        "dark-blue" : "#0A1128",
+        "red" : "#FF4D4D",
+        "disable" : "#777675",
+        "placeholder" : "#666666",
+        "placeholder-typed"  : "#393938",
+        "alert-success" : "#00C814",
+        "alert-warning" : "#FFCB45",
+        "alert-cancel"  : "#E10000",
+        
+        
+      }
+    },
   },
   plugins: [],
 }
